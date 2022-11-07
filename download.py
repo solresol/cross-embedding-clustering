ec2aa2a9280782a6476da0709e6b262c7a5c150c
Greg Baker <gregory.baker2@hdr.mq.edu.au>
2022-11-07 19:16:53 +1100

    Add word2vec and glove downloads.

--- download.py
+++ download.py
@@ -7,3 +7,7 @@ model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states = Tr
 import nltk
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+
+import gensim.downloader
+v2w_model = gensim.downloader.load('word2vec-google-news-300')
+glove_model = gensim.downloader.load('glove-twitter-25')
